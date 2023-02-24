f79c4e6161c8fb9d7ee5a759b9199ab4c9492e99
Edgar Mackevic <emackevic@avaya.com>
2023-02-24 12:29:25 +0000

    new line created

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
+        System.out.println("type below:");
         Scanner s = new Scanner(System.in);
 
         int n= s.nextInt();
